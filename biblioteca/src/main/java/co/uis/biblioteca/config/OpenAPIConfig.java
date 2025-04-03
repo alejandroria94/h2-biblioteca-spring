95ee262d87818c59b89eb45e53260d2818669393
alejandroria94 <alejandroria94@users.noreply.github.com>
2025-04-03 00:15:15 -0500

    se agrega documentacion con swagger

---
+++ biblioteca/src/main/java/co/uis/biblioteca/config/OpenAPIConfig.java
@@ -0,0 +1,18 @@
+package co.uis.biblioteca.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenAPIConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new io.swagger.v3.oas.models.info.Info()
+                        .title("Biblioteca API")
+                        .version("1.0")
+                        .description("API para gestionar una biblioteca"));
+    }
+}
